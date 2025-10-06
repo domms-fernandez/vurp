14ceee0aaacdff76927edbe97369480b10782051
domms-fernandez <119355139+domms-fernandez@users.noreply.github.com>
2025-10-06 10:49:40 -0700

    Update index.js

--- index.js
+++ index.js
@@ -55,10 +55,12 @@ function loop() {
 
     let intersect;
     if(distance(boxCenter.x, boxCenter.y, pointA.x, pointA.y) < distance(boxCenter.x, boxCenter.y, pointB.x, pointB.y)) {
-      intersect = pointA;
-    } else {
       intersect = pointB;
+    } else {
+      intersect = pointA;
     }
+    intersect.x += boxCenter.x;
+    intersect.y += boxCenter.y;
 
     sackboy.style.left = intersect.x  + "px";
     sackboy.style.top = intersect.y  + "px";
