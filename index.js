1c9a3d4897f770341f11cf7bce0f7e56c54ce0cd
domms-fernandez <119355139+domms-fernandez@users.noreply.github.com>
2025-10-06 10:37:57 -0700

    pubes crossed

--- index.js
+++ index.js
@@ -1,52 +1,5 @@
-let img = document.querySelector("img");
-let button = document.getElementById("center");
-let scream = new Audio("/gay/howie.mp3");
-
-//RUUUNNNN
-document.addEventListener("mousemove", (e) => {
-  let buttonPos = button.getBoundingClientRect(); //x and y of button
-
-  let pointA = {x: Math.round(button.clientWidth * 0.5) + 3,  y: 0}; //the x value if it intercepts the side lines,
-  let pointB = {y: Math.round(button.clientHeight * 0.5) + 3, x: 0}; //and the y value for the top and bottom lines.
-                                                                              //incidently 1/2 width/height of the button
-  //mouse X and Y relative to the center point of the button, which is (0, 0)
-  let mouseX = e.clientX - buttonPos.left - pointA.x;
-  let mouseY = e.clientY - buttonPos.top - pointB.y;
-
-  let slope = mouseY/mouseX;
-
-  if(mouseX < 0) {pointA.x *= -1; pointA.x -= 2;} //calculate intersection on the side of the button that is moving
-  if(mouseY < 0) {pointB.y *= -1; pointB.y -= 2;} //away from the mouse, so it doesn't clip across the screen
-
-  //complete points of intersection
-  pointA.y = Math.round(slope * pointA.x);
-  pointB.x = Math.round(pointB.y / slope);
-
-  //pythagoras mothafucka!
-  if((pointA.x**2 + pointA.y**2) < (pointB.x**2 + pointB.y**2)) {
-    let movementLeft = buttonPos.left - (pointA.x - mouseX);
-    if(movementLeft < 0) movementLeft += window.innerWidth;
-    if(movementLeft > window.innerWidth) movementLeft -= window.innerWidth;
-    button.style.left = movementLeft;
-
-    let movementTop = buttonPos.top - (pointA.y - mouseY);
-    if(movementTop < 0) movementTop += window.innerHeight;
-    if(movementTop > window.innerHeight) movementTop -= window.innerHeight;
-    button.style.top = movementTop;
-  } else {
-    let movementLeft = buttonPos.left - (pointB.x - mouseX);
-    if(movementLeft < 0) movementLeft += window.innerWidth;
-    if(movementLeft > window.innerWidth) movementLeft -= window.innerWidth;
-    button.style.left = movementLeft;
-
-    let movementTop = buttonPos.top - (pointB.y - mouseY);
-    if(movementTop < 0) movementTop += window.innerHeight;
-    if(movementTop > window.innerHeight) movementTop -= window.innerHeight;
-    button.style.top = movementTop;
-  }
-});
-
-/*let mousePos;
+let mousePos;
+document.addEventListener("mousemove", (e) => {mousePos = e;});
 
 function distance(x1, y1, x2, y2) {
   return Math.sqrt((x2 - x1)**2 + (y2 - y1)**2);
@@ -95,10 +48,10 @@ function loop() {
     pointA.y = slope * pointA.x;
     pointB.x = pointB.y / slope;
 
-    sackboyA.style.left = pointA.x + boxCenter.x;
-    sackboyA.style.top = pointA.y + boxCenter.y;
-    sackboyB.style.left = pointB.x + boxCenter.x;
-    sackboyB.style.top = pointB.y + boxCenter.y;
+    sackboyA.style.left = pointA.x + boxCenter.x + "px";
+    sackboyA.style.top = pointA.y + boxCenter.y + "px";
+    sackboyB.style.left = pointB.x + boxCenter.x + "px";
+    sackboyB.style.top = pointB.y + boxCenter.y + "px";
 
     let intersect;
     if(distance(boxCenter.x, boxCenter.y, pointA.x, pointA.y) < distance(boxCenter.x, boxCenter.y, pointB.x, pointB.y)) {
@@ -107,13 +60,9 @@ function loop() {
       intersect = pointB;
     }
 
-    console.log(intersect);
-
-    sackboy.style.left = 300;
-    sackboy.style.top = 300;
+    sackboy.style.left = intersect.x  + "px";
+    sackboy.style.top = intersect.y  + "px";
   }
 }
 
-document.addEventListener("mousemove", (e) => {mousePos = e; loop();});
-//setInterval(loop, 17);
-*/
+setInterval(loop, 17);
