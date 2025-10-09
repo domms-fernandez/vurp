29d3dc20271234e612e7d8164b535eb38fe0ace4
domms-fernandez <119355139+domms-fernandez@users.noreply.github.com>
2025-10-08 19:13:43 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
-const MOVEMENT_SPEED = 10; //how many multiples of its own height the body box will move
+const MOVEMENT_SPEED = 10; //how many multiples of its own height the body box will move/sec
 const FRAMERATE = 0.017; //one divided by FPS
+
+const ISAAC_HEAD_SPRITEMAP = [{
+  frames: 1,
+  duration: 1,
+  start: 0,
+  width: 28,
+  height: 25
+},
+{
+  frames: 1,
+  duration: 1,
+  start: 1,
+  width: 28,
+  height: 25
+},
+{
+  frames: 1,
+  duration: 1,
+  start: 2,
+  width: 28,
+  height: 25
+},
+{
+  frames: 1,
+  duration: 1,
+  start: 3,
+  width: 28,
+  height: 25
+}
+];
 
 const ISAAC_WALK_SPRITEMAP = [
 //idle
@@ -46,49 +76,8 @@ const ISAAC_WALK_EAST_SPRITEMAP = [
   height: 14
 }];
 
-const ISAAC_HEAD_SPRITEMAP = [{
-  frames: 1,
-  duration: 1,
-  start: 0,
-  width: 28,
-  height: 25
-},
-{
-  frames: 1,
-  duration: 1,
-  start: 1,
-  width: 28,
-  height: 25
-},
-{
-  frames: 1,
-  duration: 1,
-  start: 2,
-  width: 28,
-  height: 25
-},
-{
-  frames: 1,
-  duration: 1,
-  start: 3,
-  width: 28,
-  height: 25
-}
-];
 
-let head = document.getElementById("head");
-let box = document.getElementById("body");
-let bodyEast = document.getElementById("body-east");
-
-box.style.left = Math.floor(Math.random() * (window.innerWidth - box.clientWidth)) + "px";
-box.style.top = Math.floor(Math.random() * (window.innerHeight - box.clientHeight)) + "px";
-
-let boxRect = box.getBoundingClientRect();
-
-//snap isaac's head
-head.style.top = boxRect.top + -60 + "px";
-head.style.left = boxRect.left + -15 + "px";
-
+//spriteAnimator class
 function spriteAnimator(spritemap, element, verticallyAnimated) {
   this.spritemap = spritemap;
   this.element = element;
@@ -96,6 +85,8 @@ function spriteAnimator(spritemap, element, verticallyAnimated) {
   this.mirrored = false;
   this.selection = 0;
   this.animTime = 0;
+
+  this.update();
 }
 
 spriteAnimator.prototype.update = function() {
@@ -121,42 +112,43 @@ spriteAnimator.prototype.update = function() {
   }
 };
 
+
+let head = document.getElementById("head");
+let box = document.getElementById("body");
+let bodyEast = document.getElementById("body-east");
+
 let headAnimator = new spriteAnimator(ISAAC_HEAD_SPRITEMAP, head, false);
 let bodyAnimator = new spriteAnimator(ISAAC_WALK_SPRITEMAP, box, false);
 let bodyEastAnimator = new spriteAnimator(ISAAC_WALK_EAST_SPRITEMAP, bodyEast, true);
-bodyEastAnimator.update();
 
-let vX = 0;
-let vY = 0;
+box.style.left = Math.floor(Math.random() * (window.innerWidth - box.clientWidth)) + "px";
+box.style.top = Math.floor(Math.random() * (window.innerHeight - box.clientHeight)) + "px";
+
+let boxRect = box.getBoundingClientRect();
+
+head.style.top = boxRect.top + -60 + "px";
+head.style.left = boxRect.left + -15 + "px";
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////MAIN LOOP///////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 let mousePos;
 document.addEventListener("mousemove", (e) => {mousePos = e;});
+
+let vX = 0; let vY = 0;
 
 function loop() {
   if (!mousePos) return;
   
   let hWidth = box.clientWidth * 0.5;
   let hHeight = box.clientHeight * 0.5;
-  
   boxRect = box.getBoundingClientRect();
-  let boxCenter = {x: boxRect.left + hWidth, y: boxRect.top + hHeight};
+  let boxCenterX = boxRect.left + hWidth;
+  let boxCenterY = boxRect.top + hHeight;
   
-  const ACCELERATION = box.clientHeight * 0.2 * FRAMERATE;
-  const DECELERATION = ACCELERATION;
-  
-  if ((boxCenter.x - mousePos.clientX)**2 + (boxCenter.y - mousePos.clientY)**2 < box.clientHeight**2) {
+  if ((boxCenterX - mousePos.clientX)**2 + (boxCenterY - mousePos.clientY)**2 < hHeight**2) {
     //slowly stop
-    if(vX < 0) {
-      vX = Math.min(0, vX + DECELERATION);
-    } else {
-      vX = Math.max(0, vX - DECELERATION);
-    }
-    if(vY < 0) {
-      vY = Math.min(0, vY + DECELERATION);
-    } else {
-      vY = Math.max(0, vY - DECELERATION);
-    }
-
     headAnimator.selection = 0;
     headAnimator.animTime = 0;
     headAnimator.update();
@@ -174,48 +166,9 @@ function loop() {
     //move towards goal
     bodyEastAnimator.mirrored = false;
     
-    //step 1: goal point
-    let pointA = {x: hHeight,  y: 0}; //the x value if it intercepts the side lines,
-    let pointB = {y: hHeight, x: 0};  //and the y value for the top and bottom lines.
+    //back to square one...
+
     
-    //mouse X and Y relative to the center point of the button, which is (0, 0)
-    let relativeMouseX = mousePos.clientX - boxCenter.x;
-    let relativeMouseY = mousePos.clientY - boxCenter.y;
-
-    let slope = relativeMouseY/relativeMouseX;
-    
-    if(relativeMouseX < 0) {pointA.x *= -1; bodyEastAnimator.mirrored = true;} //calculate intersection on the side of
-    if(relativeMouseY < 0) {pointB.y *= -1;} //the button that is facing the mouse
-
-    //complete points of intersection
-    pointA.y = slope * pointA.x;
-    pointB.x = pointB.y / slope;
-
-    //choose which one is closest
-    let intersect;
-    if((pointA.x**2 + pointA.y**2) < (pointB.x**2 + pointB.y**2)) {
-      intersect = pointA;
-    } else {
-      intersect = pointB;
-    }
-
-    //step 2: move towards goal
-    //goalVX/Y is intersect * FRAMERATE * MOVEMENT_SPEED
-    //actual vX/Y slowly changes by ACCELERATION to match goalVX/Y
-    let goalVX = intersect.x * FRAMERATE * MOVEMENT_SPEED;
-    let goalVY = intersect.y * FRAMERATE * MOVEMENT_SPEED;
-
-    if(intersect.x < 0) {
-      vX = Math.max(goalVX, vX - ACCELERATION);
-    } else {
-      vX = Math.min(goalVX, vX + ACCELERATION);
-    }
-    if(intersect.y < 0) {
-      vY = Math.max(goalVY, vY - ACCELERATION);
-    } else {
-      vY = Math.min(goalVY, vY + ACCELERATION);
-    }
-
     //animate!
     //horizontal dominated
     if(Math.abs(vX) > Math.abs(vY)) {
@@ -265,6 +218,7 @@ function loop() {
   headAnimator.update();
   
 }
+  
 
 function swallow() {
   if (Math.floor(Math.random() * 20) == 0) {
