a561c319acfb5d7d317e95460c8bce5e39ef2db1
domms-fernandez <119355139+domms-fernandez@users.noreply.github.com>
2025-10-08 17:31:29 -0700

    o_0

--- index.js
+++ index.js
@@ -271,7 +271,7 @@ function swallow() {
     head.firstElementChild.src = "/vurp/img/isaac-head-pill-sheet.png";
     new Audio("/vurp/sfx/derp.x-wav").play();
   } else {
-    new Audio("/vurp/sfx/vurp.x-wav").play();
+    new Audio("/vurp/sfx/vurp.wav").play();
   }
 }
 
